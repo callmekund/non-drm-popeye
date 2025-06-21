fd051e975389e5532b98178d17025fab082e5b7b
callmekund <gauravbanna5555@gmail.com>
2025-06-21 06:19:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def show_random_emojis(message):
     emoji_message = await message.reply_text(' '.join(random.choices(emojis, k=1)))
     return emoji_message
     
-credit ="🇸 🇵 🇮 🇩 🇾" 
+credit ="𝐆𝒂ꪊ𝘳𝒂ꪜ❤️" 
 # Initialize the bot
 bot = Client(
     "bot",
@@ -458,10 +458,10 @@ async def txt_handler(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:  
-                cc = f'•——— **{a_name}** ———•\n\n——— ✨ {str(count).zfill(3)} ✨ ———\n\n🎞️𝐓𝐢𝐭𝐥𝐞 » **{name1} [{res}] .mp4**\n\n<pre><code>📚 Course : {b_name}</code></pre>\n\n🌟𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐞𝐝 𝐁𝐲 » `{CR}`\n'
-                cc1 = f'•——— **{a_name}** ———•\n\n——— ✨ {str(count).zfill(3)} ✨ ———\n\n📕𝐓𝐢𝐭𝐥𝐞 » **{name1} .pdf**\n\n<pre><code>📚 Course : {b_name}</code></pre>\n\n🌟𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐞𝐝 𝐁𝐲 » `{CR}`\n'
-                ccimg = f'•——— **{a_name}** ———•\n\n——— ✨ {str(count).zfill(3)} ✨ ———v\n\n🖼️𝐓𝐢𝐭𝐥𝐞 » **{name1} .jpg**\n\n<pre><code>📚 Course : {b_name}</code></pre>\n\n🌟𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐞𝐝 𝐁𝐲 » `{CR}`\n'
-                ccyt = f'•——— **{a_name}** ———•\n\n——— ✨ {str(count).zfill(3)} ✨ ———\n\n🎞️𝐓𝐢𝐭𝐥𝐞 » **{name1} .mp4**\n\n<a href="{url}">__**Click Here to Watch Stream**__</a>\n\n<pre><code>📚 Course : {b_name}</code></pre>\n\n🌟𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐞𝐝 𝐁𝐲 » `{CR}`\n'
+                cc = f'•——— **{a_name}** ———•\n\n——— ✨ {str(count).zfill(3)} ✨ ———\n\n🎞️𝐓𝐢𝐭𝐥𝐞 » **{name1} [{res}] .mp4**\n\n<blockquote><b>📚 Course :</b> {b_name}</blockquote>\n\n🌟𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐞𝐝 𝐁𝐲 » `{CR}`\n'
+                cc1 = f'•——— **{a_name}** ———•\n\n——— ✨ {str(count).zfill(3)} ✨ ———\n\n📕𝐓𝐢𝐭𝐥𝐞 » **{name1} .pdf**\n\n<blockquote><b>📚 Course :</b> {b_name}</blockquote>\n\n🌟𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐞𝐝 𝐁𝐲 » `{CR}`\n'
+                ccimg = f'•——— **{a_name}** ———•\n\n——— ✨ {str(count).zfill(3)} ✨ ———v\n\n🖼️𝐓𝐢𝐭𝐥𝐞 » **{name1} .jpg**\n\n<blockquote><b>📚 Course :</b> {b_name}</blockquote>\n\n🌟𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐞𝐝 𝐁𝐲 » `{CR}`\n'
+                ccyt = f'•——— **{a_name}** ———•\n\n——— ✨ {str(count).zfill(3)} ✨ ———\n\n🎞️𝐓𝐢𝐭𝐥𝐞 » **{name1} .mp4**\n\n<a href="{url}">__**Click Here to Watch Stream**__</a>\n\n<blockquote><b>📚 Course :</b> {b_name}</blockquote>\n\n🌟𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐞𝐝 𝐁𝐲 » `{CR}`\n'
                                  
                 if "drive" in url:
                     try:
